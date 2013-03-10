96f76779c022432d20453477f4fd4f45510b7592
Håkan Edling <hakan@tidyui.com>
2013-03-10 08:41:22 +0100

    Added the PiranhaCMS packages

---
+++ packages/PiranhaCMS.1.1.4/content/App_Code/WebApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Piranha.WebPages;
+
+/// <summary>
+/// Main class for initializing the application.
+/// </summary>
+public static class WebApplication
+{
+	/// <summary>
+	/// Initializes the webb app.
+	/// </summary>
+	public static void AppInitialize() {
+		WebPiranha.Init() ;
+		WebPiranha.InitServices() ;
+	}
+}
